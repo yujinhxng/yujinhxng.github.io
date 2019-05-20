b169a6d9bd713879011954e166353ee77e964bb4
Angeline Agus <angelineagus@gmail.com>
2019-05-21 01:29:00 +0700

    base1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YUJINHXNG - Home</title>
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+	<!-- JS  -->
+	<link href="js/jquery-3.4.1.min.js">
+	<!-- JS - TEXTILLATE -->
+	<link href="js/textillate-master/jquery.textillate.js">
+</head>
+<body>
+	<h1 class="tlt">
+		<ul class="texts">
+			<li data-out-effect="fadeOut" data-out-shuffle="true"></li>
+			<li data-in-effect="fadeIn">Angeline</li>
+		</ul>
+	</h1>
+
+	<script type="text/javascript">
+		$(".tlt").textillate();
+	</script>
+</body>
+</html>
